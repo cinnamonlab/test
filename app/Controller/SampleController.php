9f4e6fd7f196a9c7717dc12f9d487036e1b7670b
hajime3333out <hotta@fastbid.asia>
2015-08-07 22:07:09 +0900

    SQL test code

--- app/Controller/SampleController.php
+++ app/Controller/SampleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 use Framework\Config;
+use Framework\DB\DB;
 use Framework\Response;
 use Framework\Input;
 
@@ -11,5 +12,25 @@ class SampleController {
             ->setContent(Config::get('app.greeting') . Input::get('name', 'guest'))
             ->setContentType('text/plain')
             ->setCode(200);
+    }
+
+    function db( ) {
+        $result = DB::query("select * from user limit 10");
+        $message = print_r($result, true);
+
+        $result = DB::connection('write')
+            ->select('select * from user where created_at > interval ? days limit ?', 100, 2 );
+        $message .= print_r($result, true);
+
+        $result = DB::connection('write')
+            ->select('select * from user where created_at > interval :interval days limit :limit',
+                array(':limit'=>5, ':interval'=>100) );
+        $message .= print_r($result, true);
+
+        return (new Response())
+            ->setContent($message)
+            ->setContentType('text/plain')
+            ->setCode(200);
+
     }
 }
